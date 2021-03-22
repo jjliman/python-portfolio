bb29e0eb6893781e2acb0f3dc83841b3e813b19e
Justin Liman <justinliman97@gmail.com>
2021-03-22 00:43:35 -0700

    my website portfolio

---
+++ server.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, redirect
+import csv
+
+
+app = Flask(__name__)
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'{email},{subject},{message}\n')
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+@app.route('/blog')
+def blog():
+    return 'Haello, Blog!'
+
+
+@app.route('/blog/2020/cows')
+def blog2():
+    return 'Haello, Cows!'
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('/thankyou.html')
+        except Exception:
+            return 'did not save to database'
+    else:
+        return 'error!!!'
